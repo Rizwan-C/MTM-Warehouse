ced0abdc62bcb4e18aa79d2d6d5fc19a599a3086
CaptainnLevii <rizwan.chaudhari.ca@gmail.com>
2024-03-25 03:28:15 -0400

    Add project files.

---
+++ MTM-Warehouse/Entities/LoginEmp.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MTM_Warehouse.Entities
+{
+    public class LoginEmp
+    {
+        [Key]
+        public int LoginEmpId { get; set; }
+
+        [Required(ErrorMessage ="")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? AccessRights { get; set; }
+
+    }
+}
---
+++ MTM-Warehouse/Entities/WarehouseInfo.cs
@@ -0,0 +1,33 @@
+﻿using Mono.TextTemplating;
+using System.ComponentModel.DataAnnotations;
+
+namespace MTM_Warehouse.Entities
+{
+    public class WarehouseInfo
+    {
+
+        [Key]
+        public int W_Id { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? W_Name { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? W_Location { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public string? W_PinCode { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public double? W_TotalCapacity { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public double? W_SpaceAvailable { get; set; }
+
+        [Required(ErrorMessage = "")]
+        public double? W_PercentFull { get; set; }
+
+
+
+    }
+}
