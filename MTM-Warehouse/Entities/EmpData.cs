fafa7a1f5703b8fef73ca44565e2c9d329a8a295
CaptainnLevii <rizwan.chaudhari.ca@gmail.com>
2024-04-02 08:04:36 -0400

    Update Database

--- MTM-Warehouse/Entities/EmpData.cs
+++ MTM-Warehouse/Entities/EmpData.cs
@@ -8,26 +8,21 @@ namespace MTM_Warehouse.Entities
         [Key]
         public int EmpDataId { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? Name { get; set; }
+        [Required(ErrorMessage = "Please enter Name")]
+        public string? Name { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? Email { get; set; }
+        [Required(ErrorMessage = "Please enter Email")]
+        public string? Email { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? Phone { get; set; }
+        [Required(ErrorMessage = "Please enter Phone #")]
+        public string? Phone { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? Address { get; set; }
+        [Required(ErrorMessage = "Please enter Address")]
+        public string? Address { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? PinCode { get; set; }
+        [Required(ErrorMessage = "Please enter Pincode")]
+        public string? PinCode { get; set; }
 
-        [Required(ErrorMessage = "")]
-        public double? FirstDay { get; set; }
-
-        [Required(ErrorMessage = "")]
-        public double? LastDay { get; set; }
 
         
         public int? WarehouseInfoId { get; set; }
---
+++ MTM-Warehouse/Migrations/20240402114817_DatatypeChangeINEmpData.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MTM_Warehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatatypeChangeINEmpData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstDay",
+                table: "EmpData_DbData");
+
+            migrationBuilder.DropColumn(
+                name: "LastDay",
+                table: "EmpData_DbData");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PinCode",
+                table: "EmpData_DbData",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "EmpData_DbData",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "EmpData_DbData",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "EmpData_DbData",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "EmpData_DbData",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "PinCode",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Phone",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Name",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Email",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Address",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<double>(
+                name: "FirstDay",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LastDay",
+                table: "EmpData_DbData",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
